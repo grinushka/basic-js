7e7fc4f62ab4cd25d5ef49292263ba87a0218f41
grinushka <vilu.badgley@gmail.com>
2022-10-16 09:35:40 +0700

    transform array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -15,8 +15,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (n == 0)
+        {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        document.write("Move disk " + n + " from rod " + from_rod +
+        " to rod " + to_rod+"<br/>");
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
+  let arrDuplicate = arr;
+
+  for(let i = 0; i < arrDuplicate.length; i++) {
+    if (arrDuplicate[i] === '--discard-next') {
+      if(arrDuplicate[i+1]) {
+        arrDuplicate.splice(i, 2);
+        i--;
+        // console.log(arrDuplicate)
+      } else {arrDuplicate.splice(i, 1)}
+    } else if (arrDuplicate[i] === '--discard-prev') {
+      if (arrDuplicate[i-1]) {
+        arrDuplicate.splice(i-1, 2);
+        i = i - 2 ;
+        // console.log(arrDuplicate)
+      } else {
+        arrDuplicate.splice(i, 1);
+        i--
+      }
+    } else if (arrDuplicate[i] === '--double-next') {
+      if (arrDuplicate[i+1]) {
+        arrDuplicate.splice(i, 1, arrDuplicate[i+1])
+        // console.log(arrDuplicate)
+      } else {arrDuplicate.splice(i, 1);}
+           
+    } else if (arrDuplicate[i] === '--double-prev') {
+      if (arrDuplicate[i-1]) {
+        arrDuplicate.splice(i, 1, arrDuplicate[i-1])
+      } else {
+        arrDuplicate.splice(i, 1); 
+        i--}
+      
+        // console.log(arrDuplicate)
+    }
+  };
+
+  console.log(arrDuplicate);
+  return arrDuplicate
+  
 }
 
 module.exports = {
